1549cf3e897b6f27ea19047fbd9c9f1c451b8862
Sami <b.sami.code@gmail.com>
2024-11-10 16:12:55 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -11,18 +11,23 @@ var _ = net.Listen
 var _ = os.Exit
 
 func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
 
-	// Uncomment this block to pass the first stage
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
 		fmt.Println("Failed to bind to port 4221")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+	conn, err := l.Accept()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+	// Print connection details
+	fmt.Println("Accepted new connection:")
+	fmt.Println("Remote Address:", conn.RemoteAddr()) // Remote (client) address
+	fmt.Println("Local Address:", conn.LocalAddr())   // Local (server) address
+
+	// Optional: Print the connection directly (may provide limited details)
+	fmt.Printf("Conn object: %v\n", conn)
 }
