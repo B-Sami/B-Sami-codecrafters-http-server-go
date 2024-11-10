2775a439775c4fecd39a3ad200d02cd9037cb04b
Sami <b.sami.code@gmail.com>
2024-11-10 18:45:40 +0100

    codecrafters submit [skip ci]

--- app/server.go
+++ app/server.go
@@ -3,31 +3,114 @@ package main
 import (
 	"fmt"
 	"net"
+	"net/http"
 	"os"
 )
 
-// Ensures gofmt doesn't remove the "net" and "os" imports above (feel free to remove this!)
-var _ = net.Listen
-var _ = os.Exit
+type Server struct {
+	Network string
+    Addr    string
+    Handler http.Handler
+}
+
+type StatusLine struct {
+	HTTPVersion  string
+	StatusCode   int   
+	StatusPhrase string
+}
+type HTTPResponse struct {
+	StatusLine StatusLine
+	Headers map[string]string
+	ResponseBody string
+}
+
+func NewServerResponse() *HTTPResponse {
+	return &HTTPResponse{
+		StatusLine: StatusLine{
+			HTTPVersion:  "HTTP/1.1",
+			StatusCode:   200,
+			StatusPhrase: "OK",
+		},
+		Headers:      make(map[string]string),
+		ResponseBody: "",
+	}
+}
+
+func (r *HTTPResponse) FormatStatusLine() string {
+	return fmt.Sprintf("%s %d %s\r\n", r.StatusLine.HTTPVersion, r.StatusLine.StatusCode, r.StatusLine.StatusPhrase)
+}
+
+func (r *HTTPResponse) FormatHeaders() string {
+	var headers string
+	for key, value := range r.Headers {
+		headers += fmt.Sprintf("%s: %s\r\n", key, value)
+	}
+	if headers == "" {
+		return "\r\n"
+	}
+	return headers + "\r\n"
+}
+
+func (r *HTTPResponse) FormatBody() string {
+	return r.ResponseBody
+}
+
+func (r *HTTPResponse) FullResponse() string {
+	return r.FormatStatusLine() + r.FormatHeaders() + r.FormatBody()
+}
 
 func main() {
 	fmt.Println("Logs from your program will appear here!")
 
-	l, err := net.Listen("tcp", "0.0.0.0:4221")
-	if err != nil {
-		fmt.Println("Failed to bind to port 4221")
-		os.Exit(1)
+    server := &Server{
+		Network: "tcp",
+		Addr:    ":4221",
+		Handler: http.DefaultServeMux,
 	}
-	conn, err := l.Accept()
-	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
-	}
-	// Print connection details
-	fmt.Println("Accepted new connection:")
-	fmt.Println("Remote Address:", conn.RemoteAddr()) // Remote (client) address
-	fmt.Println("Local Address:", conn.LocalAddr())   // Local (server) address
 
-	// Optional: Print the connection directly (may provide limited details)
-	fmt.Printf("Conn object: %v\n", conn)
+	if err := server.ListenAndServe(server.Addr); err != nil {
+		fmt.Println("Error starting server:", err)
+	}
+}
+
+func (s *Server) ListenAndServe(address string) error {
+	fmt.Println("Starting server on", address)
+	listener, err := net.Listen(s.Network, address)
+	if err != nil {
+		fmt.Println("Failed to bind to", address, "Error:", err)
+		os.Exit(1)
+	}
+	defer listener.Close()
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+		fmt.Println("Connection accepted:", conn.RemoteAddr())
+		go handleRequest(conn)
+
+	}
+}
+
+func handleRequest(conn net.Conn) {
+	buf := make([]byte, 1024)
+	_, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading request:", err)
+		conn.Close()
+		return
+	}
+
+	response := NewServerResponse()
+
+	_, err = conn.Write([]byte(response.FullResponse()))
+	if err != nil {
+		fmt.Println("Error sending response:", err)
+		conn.Close()
+		return
+	}
+
+	conn.Close()
 }
